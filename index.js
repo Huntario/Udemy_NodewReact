02223f37e019d6845233c22161725b247e9bd091
Hunter Leachman <leachmanh@gmail.com>
2019-03-19 20:54:49 -0500

    28 complete auth process

--- index.js
+++ index.js
@@ -8,9 +8,11 @@ const app = express();
 passport.use(new GoogleStrategy({
   clientID: keys.googleClientID,
   clientSecret: keys.googleClientSecret,
-  callBackURL: '/auth/google/callback'
-  }, (accessToken) => {
+  callbackURL: '/auth/google/callback'
+  }, (accessToken, refreshToken, profile, done) => {
     console.log(accessToken);
+    console.log(refreshToken);
+    console.log(profile);
   })
 );
 
@@ -19,6 +21,9 @@ app.get('/auth/google',
 	scope:['profile','email']
   })
 );
+
+app.get('/auth/google/callback', 
+	passport.authenticate('google'));
 
 
 const PORT = process.env.PORT || 5000;
